0cd21d71c56b1858b7399ac57850d6e002be160d
lguzmanc <lguzmanc@frd.org.mx>
2023-05-05 10:07:45 -0600

    deleting trash code

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -9,7 +9,6 @@ const environment_config = conexion['conexion']
  * DB Config
  */
 export const SQLDB_CONFIG = {
-    // SQL_SERVER: DEV ? /* '54.219.228.138' */ '127.0.0.1' : process.env.SQL_HOST || '172.32.50.13',
     SQL_SERVER: process.env.ENV_SERVER || environment_config.database_config.server,
     SQL_USER: desencriptar(process.env.ENV_USER || environment_config.database_config.user),
     SQL_PASS: desencriptar(process.env.ENV_PASS || environment_config.database_config.password),
